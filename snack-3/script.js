361832db18288d9c009a5d29d02153f3ca2b725a
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-12 14:13:05 +0100

    feat: snack3 completato

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,9 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+let newNumbers=numbers.map((element)=>{
+    return element + 1
+})
+console.log(newNumbers)
+
 // Risultato: [3, 9, 5, 8, 3, 88]
