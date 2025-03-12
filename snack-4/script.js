dcd109e28209a7970d032c704ea76a483fc8338f
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-12 14:15:45 +0100

    feat: snack4 completato

--- snack-4/script.js
+++ snack-4/script.js
@@ -6,4 +6,8 @@ const posts = [
 ]
 
 // Crea un nuovo array coi nomi di tutti gli autori
+let authors = posts.map((element)=>{
+  return element.author
+})
+console.log(authors)
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
