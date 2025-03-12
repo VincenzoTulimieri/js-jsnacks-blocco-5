ec76daf1c4af94c575f10dda37bb19feae2b7ee5
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-12 13:45:18 +0100

    feat: snack1 fatto

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,7 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+names.forEach(elemnt =>{
+    console.log(elemnt)
+})
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
