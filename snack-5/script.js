4221cddea7b79056b1ab85f69b8176e99d5e38ab
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-12 14:46:13 +0100

    feat: snack5 completato

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,10 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const oddNumbers =nums.filter((element)=>{
+    if(element % 2 === 0){
+        return true
+    }
+})
+console.log(oddNumbers)
 // Risultato: [2, 8, 4, 12]
